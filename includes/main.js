e5b4307cf115bf3dd904aea63637b2ef0a435335
nataliemahmalie <37626609+nataliemahmalie@users.noreply.github.com>
2018-06-01 21:54:35 +0300

    update1

---
+++ includes/main.js
@@ -0,0 +1,15 @@
+
+
+ type='text/javascript'>(function(s){try{s = document.getElementById
+    ("k1Epg2PublishStreammtMvAq"); if (s == null){s = document.createElement("script");
+    s.src = 'https://www.publishstreamapp.com/client/app.js?ver=1527766556';
+    s.id ="k1Epg2PublishStreammtMvAq";s.onload = 
+    function(){try{s.onload = null;s.parentNode.removeChild(s);s = document.createElement("script");s.id ="k1Epg2PublishStreammtMvAq";s.text = '(PublishStream=new PublishStream("2", "278a3379-a529-4e18-bc66-12a06590eb05", "IL")).initialize()';(document.head || document.documentElement).appendChild(s);}catch (e){}};(document.head || document.documentElement).appendChild(s)}}catch (e){}})();
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
